e8291bda4209a29ec4f16a15a28e0d1a8f994f05
Vlad Postoroanca <vladi@192.168.1.137>
2025-08-07 15:06:57 +0300

    Create __main__.py to enable 'python -m loan_analysis' execution

--- src/loan_analysis/__main__.py
+++ src/loan_analysis/__main__.py
@@ -1,5 +1,12 @@
+from loan_analysis.cli import parse_and_validate
+from loan_analysis.core import run_calculation
+
 def main():
-    print("LoanAnalysis is working!")
+    args = parse_and_validate()
+    run_calculation(args)
 
 if __name__ == "__main__":
     main()
+
+# Run code:
+# python -m src.loan_analysis
